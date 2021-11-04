8443a8f6b09fa198db71b2ffa1684816e7cb146a
adu <adu@nemlig.com>
2021-11-04 12:51:56 +0100

    implemented sensitive logging.

--- IMDBdataservice/imdbContext.cs
+++ IMDBdataservice/imdbContext.cs
@@ -42,6 +42,8 @@ namespace IMDBdataservice
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.LogTo(Console.WriteLine, Microsoft.Extensions.Logging.LogLevel.Information);
+                optionsBuilder.EnableSensitiveDataLogging();
                 optionsBuilder.UseNpgsql("host=localhost;database=imdb;Username=postgres;Password=postgres");
             }
         }
