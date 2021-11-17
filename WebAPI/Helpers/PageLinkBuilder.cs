c97ed92db63d53b6ef92c1199ebf8584bb0e8f47
Anton D <adu@nemlig.com>
2021-11-17 14:54:05 +0100

    Fix paging

--- WebAPI/Helpers/PageLinkBuilder.cs
+++ WebAPI/Helpers/PageLinkBuilder.cs
@@ -8,20 +8,20 @@ namespace WebAPI.Controllers
     {
 
 
-        public Uri FirstPage { get; private set; }
-        public Uri LastPage { get; private set; }
-        public Uri NextPage { get; private set; }
-        public Uri PreviousPage { get; private set; }
+        public Uri FirstPage { get; set; }
+        public Uri LastPage { get; set; }
+        public Uri NextPage { get; set; }
+        public Uri PreviousPage { get; set; }
 
         public PageLinkBuilder(IUrlHelper url, string route, object extraValues, int pageNum, int pageSize, long total)
         {
             Console.WriteLine(total);
+            Console.WriteLine(pageNum);
             // Determine total number of pages
             var pageCount = total > 0
                 ? (int)Math.Ceiling(total / (double)pageSize)
                 : 0;
-
-            // Create them page links 
+ 
             FirstPage = new Uri(url.Link(route, new HttpRouteValueDictionary(extraValues)
             {
                 {"page", 1},
@@ -35,18 +35,18 @@ namespace WebAPI.Controllers
             if (pageNum > 1)
             {
                 PreviousPage = new Uri(url.Link(route, new HttpRouteValueDictionary(extraValues)
-            {
-                {"page", pageNum - 1},
-                {"pagesize", pageSize}
-            }));
+                {
+                    {"page", pageNum - 1},
+                    {"pagesize", pageSize}
+                }));
             }
             if (pageNum < pageCount)
             {
                 NextPage = new Uri(url.Link(route, new HttpRouteValueDictionary(extraValues)
-            {
-                {"page", pageNum + 1},
-                {"pagesize", pageSize}
-            }));
+                {
+                    {"page", pageNum + 1},
+                    {"pagesize", pageSize}
+                }));
             }
         }
     }
